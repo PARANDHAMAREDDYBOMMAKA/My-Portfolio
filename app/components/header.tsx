5f134b8811403c1465aea7e28514e374efb96da7
PARANDHAMAREDDYBOMMAKA <rparandhama63@gmail.com>
2024-09-05 16:33:15 +0530

    Made some css improvments

--- app/components/header.tsx
+++ app/components/header.tsx
@@ -1,13 +1,13 @@
 import Link from "next/link";
 
 const Header: React.FC = () => (
-  <header className="bg-black bg-opacity-60 backdrop-blur-md text-white fixed w-[40%] top-0 mx-[30%] py-2 my-4 rounded-2xl z-50 shadow-lg">
-    <nav className="flex justify-center items-center py-4">
-      <ul className="flex space-x-8">
+  <header className="bg-black bg-opacity-60 backdrop-blur-md text-white fixed top-0 left-1/2 transform -translate-x-1/2 w-[50%] py-6 rounded-2xl z-50 shadow-lg mt-4">
+    <nav className="flex flex-wrap justify-center items-center px-4">
+      <ul className="flex flex-wrap justify-center space-x-4 text-center">
         <li>
           <Link
             href="#about"
-            className="hover:text-purple-500 transition-colors duration-300"
+            className="hover:text-purple-500 transition-colors duration-300 text-sm sm:text-base md:text-lg"
           >
             About
           </Link>
@@ -15,7 +15,7 @@ const Header: React.FC = () => (
         <li>
           <Link
             href="#techstacks"
-            className="hover:text-purple-500 transition-colors duration-300"
+            className="hover:text-purple-500 transition-colors duration-300 text-sm sm:text-base md:text-lg"
           >
             Tech Stacks
           </Link>
@@ -23,7 +23,7 @@ const Header: React.FC = () => (
         <li>
           <Link
             href="#projects"
-            className="hover:text-purple-500 transition-colors duration-300"
+            className="hover:text-purple-500 transition-colors duration-300 text-sm sm:text-base md:text-lg"
           >
             Projects
           </Link>
@@ -31,7 +31,7 @@ const Header: React.FC = () => (
         <li>
           <Link
             href="#contact"
-            className="hover:text-purple-500 transition-colors duration-300"
+            className="hover:text-purple-500 transition-colors duration-300 text-sm sm:text-base md:text-lg"
           >
             Contact
           </Link>
--- app/components/heroSection.tsx
+++ app/components/heroSection.tsx
@@ -6,7 +6,7 @@ const HeroSection: React.FC = () => {
   return (
     <section
       id="home"
-      className="flex flex-col items-center justify-center h-screen bg-gradient-to-b from-blue-900 via-purple-800 to-indigo-700 text-white text-center"
+      className="flex flex-col items-center justify-center h-screen bg-gradient-to-b from-blue-900 via-purple-800 to-indigo-700 text-white text-center p-4"
     >
       <div className="flex flex-col items-center mb-8">
         <h1 className="text-5xl font-extrabold mb-4  animate-pulse">
@@ -23,8 +23,7 @@ const HeroSection: React.FC = () => {
           )}
         </h1>
         <p className="text-2xl mb-6 text-gray-400">
-          Hi! I'm Parandhama Reddy, a Full Stack Developer based in Andhra
-          Pradesh.
+          Hi! I'm Parandhama Reddy, a Full Stack Developer.
         </p>
         <Link href="#projects">
           <p className="bg-purple-500 text-white px-6 py-3 rounded-lg border-2 border-transparent transition duration-300 hover:border-3 hover:border-gradient-to-r hover:from-red-500 hover:via-yellow-500 hover:to-green-500">
