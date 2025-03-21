16fff288834ae8c18057c1174cf506e8c1985d54
Parandhama Reddy Bommaka <149254670+PARANDHAMAREDDYBOMMAKA@users.noreply.github.com>
2025-03-21 11:57:32 +0530

    Updated projects

--- app/utils/data.ts
+++ app/utils/data.ts
@@ -22,5 +22,27 @@ export const projects: Project[] = [
     link: "https://client-ten-navy.vercel.app/",
     imageUrl: "https://firebasestorage.googleapis.com/v0/b/capstone-46186.appspot.com/o/Screenshot%202024-08-31%20at%202.41.09%E2%80%AFPM.png?alt=media&token=01312103-0d82-4641-806e-d13b901bdccd",
   },
+  {
+    id: 3,
+    title: "Converse",
+    description: "A social networking platform that connects people with similar interests and provides a safe space for meaningful conversations.",
+    link: "https://chat-app-silk-nine.vercel.app/",
+    imageUrl: "https://firebasestorage.googleapis.com/v0/b/capstone-46186.appspot.com/o/Screenshot%202025-03-21%20at%2011.12.26%E2%80%AFAM.png?alt=media&token=dea37517-ca2a-438f-894c-08e57d529df8",
+  },
+  {
+    id: 4,
+    title: "Claims Management",
+    description: "A web application that simplifies the process of managing insurance claims for both patients and insurance companies.",
+    link: "https://minimal-claims.vercel.app/",
+    imageUrl: "https://firebasestorage.googleapis.com/v0/b/capstone-46186.appspot.com/o/Screenshot%202025-03-21%20at%2011.14.48%E2%80%AFAM.png?alt=media&token=7ec9c700-e944-4224-961c-f87ec6e8d8a1",
+  },
+  {
+    id: 5,
+    title: "Library management",
+    description: "A platfrom where the library can manage their books and users can borrow books.",
+    link: "https://library-management-gamma-nine.vercel.app/",
+    imageUrl: "https://firebasestorage.googleapis.com/v0/b/capstone-46186.appspot.com/o/Screenshot%202025-03-21%20at%2011.17.18%E2%80%AFAM.png?alt=media&token=b40c321e-11dd-42d5-a407-fbf683a7a59e",
+  },
+
   // Add more projects as needed
 ];
